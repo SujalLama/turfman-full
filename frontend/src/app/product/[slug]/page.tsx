40d19cacb2e3e8e83189644cf40f888606f9d576
sujal lama <lsujal41@gmail.com>
2023-11-09 13:58:53 +0545

    error component

--- frontend/src/app/product/[slug]/page.tsx
+++ frontend/src/app/product/[slug]/page.tsx
@@ -1,6 +1,8 @@
+import FaIcons from "@/components/FaIcons";
 import RelatedProducts from "@/section/RelatedProducts"
 import SingleProductContent from "@/section/SingleProductContent"
 import { formatProduct } from "@/utils/dataFormatter"
+import Link from "next/link";
 
 export async function generateStaticParams() {
   try {
@@ -37,12 +39,12 @@ async function getProductData (slug: string) {
     const {data} = await fetch(url, {cache: 'no-store'}).then((res) => res.json());
    
      if(!data) {
-      throw new Error('Failed to fetch data')
+      return null;
      }
    
      return formatProduct(data[0]);
   } catch(error) {
-    throw new Error('Failed to fetch data')
+    return null;
   }
 }
 
@@ -51,7 +53,20 @@ export default async function page({params}: {params: {slug: string;}}) {
   
 
   if(!product) {
-    return null;
+    return (
+        <main className="bg-gray/20 xl:-mt-7.5">
+            <div className="py-40 px-7.5 mx-auto relative z-10 sm:max-w-[540px] md:max-w-[720px] large:max-w-[960px]  xl:px-3.5 xl:max-w-[1200px]">
+                <div className="flex items-center justify-center flex-col text-center">
+                    <h2 className="leading-[1.1] text-primary text-[100px] font-bold">404</h2>
+                    <h3 className="font-semibold text-2xl text-gray-darker mb-10">Oops! The page you were looking for, couldn&apos;t be found.</h3>
+                    <Link  href="/" className="text-gray-darker hover:text-primary transition-colors duration-500 ease-in-out">
+                        <span className="pr-2">Back to home page</span>
+                        <FaIcons icon="faArrowRight" />
+                    </Link>
+                </div>
+            </div>
+        </main>
+    );
   }
 
   return (
